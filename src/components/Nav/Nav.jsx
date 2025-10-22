9b2d4c18206a498b55ca0f9d4ebe8f7fb84a8792
antonyperalta-hug <mariavaleria2202@gmail.com>
2025-10-21 23:09:45 -0300

    first commit

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import { useCartContext } from "../../context/CartContext/useCartContext";
+
+import "./Nav.css";
+
+export const Nav =  () => {
+    const { getTotalItems } = useCartContext();
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to={"/"}>Home</Link>
+                </li>
+                <li>
+                    <Link to={"/category/dulce"}>Pasteleria</Link>
+                </li>
+                <li>
+                    <Link to={"/category/salado"}>Panaderia</Link>
+                </li>
+                <li>
+                    <Link>Carrito</Link>
+                    {getTotalItems() > 0 && (
+                        <span className="in-cart">{getTotalItems()}</span>
+                    )}
+                </li>
+            </ul>
+        </nav>
+    );
+};
