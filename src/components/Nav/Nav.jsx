b61565732af182edc38c91c32d826552626e2ef4
antonyperalta-hug <mariavaleria2202@gmail.com>
2025-10-22 19:52:19 -0300

    Add files via upload
    actualizo estilos

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,28 +1,27 @@
 import { Link } from "react-router-dom";
 import { useCartContext } from "../../context/CartContext/useCartContext";
-
 import "./Nav.css";
 
-export const Nav =  () => {
+export const Nav = () => {
     const { getTotalItems } = useCartContext();
 
     return (
-        <nav>
-            <ul>
+        <nav className="navbar">
+            <ul className="nav-links">
                 <li>
-                    <Link to={"/"}>Home</Link>
+                    <Link to="/">Home</Link>
                 </li>
                 <li>
-                    <Link to={"/category/dulce"}>Pasteleria</Link>
+                    <Link to="/category/dulce">Pastelería</Link>
                 </li>
                 <li>
-                    <Link to={"/category/salado"}>Panaderia</Link>
+                    <Link to="/category/salado">Panadería</Link>
                 </li>
-                <li>
-                    <Link>Carrito</Link>
+                <li className="cart-link">
+                    <Link to="/cart">Carrito</Link>
                     {getTotalItems() > 0 && (
                         <span className="in-cart">{getTotalItems()}</span>
-                    )}
+                )}
                 </li>
             </ul>
         </nav>
