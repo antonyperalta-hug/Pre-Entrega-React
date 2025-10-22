ee8e7272c7fd81f1280c6454f8a4e4731215d5a6
antonyperalta-hug <mariavaleria2202@gmail.com>
2025-10-22 19:49:14 -0300

    Actualizo el footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,18 +3,18 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <footer>
-            <p>Derechos Reservados © 2025 - E-Shop.</p>
+            <p>Derechos Reservados © 2025 - Nathy Pasteleria.</p>
             <div className="footer-items-container">
                 <a href="https://www.facebook.com/" target="_blank">
-                    <img src="/img/common/logo-facebook.png" alt="logo-facebook" />
+                    <img src="../../../public/images/logo-facebook.png" alt="logo-facebook" />
                 </a>
 
                 <a href="https://www.instagram.com/" target="_blank">
-                    <img src="/img/common/logo-instagram.png" alt="logo-instagram" />
+                    <img src="../../../public/images/logo-instagram.png" alt="logo-instagram" />
                 </a>
 
                 <a href="https://www.whatsapp.com/?lang=es_LA" target="_blank">
-                    <img src="/img/common/logo-whatsapp.png" alt="logo-whatsapp" />
+                    <img src="../../../public/images/logo-whatsapp.png" alt="logo-whatsapp" />
                 </a>
             </div>
         </footer>
