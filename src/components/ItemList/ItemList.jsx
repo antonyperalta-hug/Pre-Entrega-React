84d28a4d91ecb614ed1017cd2c36945ac2825dae
antonyperalta-hug <mariavaleria2202@gmail.com>
2025-10-22 19:50:50 -0300

    Add files via upload
    actualizo estilo item list

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,18 +1,22 @@
 import { Link } from "react-router-dom";
 import { Item } from "../Item/Item";
+import "./ItemList.css"; 
 
 export const ItemList = ({ list }) => {
     return (
-        <>
-            {list.length ? (
-                list.map((prod) => (
-                    <Link to={`/detail/${prod.id}`} key={prod.id}>
-                        <Item {...prod} />
-                </Link>
-                ))
-            ) : (
-                <p>No hay productos</p>
-            )}
-        </>
-    );
+    <div className="item-list">
+        {list.length ? (
+        list.map((prod) => (
+        <Link
+            to={`/detail/${prod.id}`}
+            key={prod.id}
+            className="item-card"
+        >
+            <Item {...prod} />
+            </Link>
+        ))
+        ) : (
+        <p>No hay productos</p> )}
+    </div>);
 };
+
