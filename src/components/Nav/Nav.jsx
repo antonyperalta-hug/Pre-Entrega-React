7babdbc0252b85681adc6bf6d165d805b0abaa5b
antonyperalta-hug <mariavaleria2202@gmail.com>
2025-10-25 10:27:03 -0300

    Actualiza css Nav
    Actualiza css Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom";
 import { useCartContext } from "../../context/CartContext/useCartContext";
 import "./Nav.css";
+import logo from "../../../public/images/logo.jpg";
+
 
 export const Nav = () => {
     const { getTotalItems } = useCartContext();
 
     return (
         <nav className="navbar">
+            <div className="nav-logo">
+                <Link to="/">
+                    <img src={logo}  alt="Logo"/>
+                </Link>
+            </div>
             <ul className="nav-links">
                 <li>
                     <Link to="/">Home</Link>
